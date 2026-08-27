bac26e688a83031a21668f32b9663e736dc0d94b
Arshia Sirohi <141633236+arshia1901@users.noreply.github.com>
2026-08-27 13:58:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        //vector to store values from 0 to n 
-        //dp[i] represents number of ways to climb i stairs 
-        vector<int> dp(n+1); 
-        for(int i = 0; i<=n; i++){
-            if(i==0 || i == 1){
-                //for 0th stair there is only 1 way- do nothing 
-                //for 1st stair there is only 1 way - from 0th stairs
-                dp[i]=1;
-            }
-            else{
-                //for ith stair- we can come from i-1 stair or i-2 stair 
-                dp[i]=dp[i-1]+dp[i-2];
-            }
+        vector<int> dp(n+1, 0); 
+        if(n<=1){
+            return 1;
         }
-        return dp[n];
+        int prev2=1; 
+        int prev=1; 
+        int curr;
+        for(int i = 2; i<=n; i++){
+            curr = prev+prev2; 
+            prev2=prev; 
+            prev=curr;  
+        }
+        return prev;
     }
 };

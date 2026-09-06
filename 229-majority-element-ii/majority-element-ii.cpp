31059bed665314a5410a6b3e5b03d0123b8d3917
Arshia Sirohi <141633236+arshia1901@users.noreply.github.com>
2026-09-06 15:40:43 +0530

    Time: 3 ms (50.98%) | Memory: 23.8 MB (50.40%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,16 +2,47 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size(); 
-        vector<int> result; 
-        unordered_map<int, int> mpp; 
+        int el1=INT_MIN; 
+        int el2= INT_MIN; 
+        int cnt1=0, cnt2=0; 
         for(int i = 0; i<n; i++){
-            mpp[nums[i]]++; 
-        }
-        int freq = n/3; 
-        for(auto num : mpp){
-            if(num.second>freq){
-                result.push_back(num.first); 
+            if(cnt1==0 && nums[i]!=el2){
+                el1 = nums[i]; 
+                cnt1=1; 
             }
+            else if(cnt2==0 && nums[i]!= el1){
+                el2 = nums[i]; 
+                cnt2=1;
+            }
+            else if(nums[i]==el1){
+                cnt1++; 
+            }
+            else if(nums[i]==el2){
+                cnt2++; 
+            }
+            else{
+                cnt1--; 
+                cnt2--;
+            }
+        }
+        int count1=0; 
+        int count2=0;
+        vector<int> result; 
+        for(int x : nums){
+            if(x==el1){
+                count1++; 
+            }
+        }
+        if(count1> n/3){
+            result.push_back(el1); 
+        }
+        for(int x:nums){
+            if(x==el2){
+                count2++; 
+            }
+        }
+        if(count2>n/3){
+            result.push_back(el2); 
         }
         return result; 
     }

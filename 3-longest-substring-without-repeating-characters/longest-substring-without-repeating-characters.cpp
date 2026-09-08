e300a0bd0595bb66334061561ad913616590500c
Arshia Sirohi <141633236+arshia1901@users.noreply.github.com>
2026-09-08 13:17:14 +0530

    Time: 299 ms (19.85%) | Memory: 81.4 MB (17.39%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //variable sliding window
-        //to check for duplicates use set 
-        int l=0; 
-        int ans=0; 
-        unordered_set<char> st;
-        for(int r=0; r<s.size(); r++){
-            while(st.count(s[r])){
-                st.erase(s[l]);
-                l++;
+        int n = s.size(); 
+        int len = 0; 
+        int l =0; 
+        unordered_set<char> st; 
+        for(int r=0; r<n; r++){
+            while(st.find(s[r])!=st.end()){
+                st.erase(s[l]); 
+                l++; 
             }
-            st.insert(s[r]);
-            ans = max(ans, r-l+1);
+            st.insert(s[r]); 
+            len = max(len, r-l+1); 
         }
-        return ans;
+        return len; 
     }
 };
